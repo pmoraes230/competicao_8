a2f2505cd207f6701a0c0479f5c678f7c413d809
pmoraes230 <pmoraes230nascimento@gmail.com>
2025-08-22 17:46:55 -0300

    urls

--- home/urls.py
+++ home/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     # Event
     path("deteils_event/<int:id_event>/", views.deteils_event, name="deteils_event"),
     path("buy_ticket/<int:id_event>/", views.buy_ticket, name="buy_ticket"),
-    path("list_tickets/<int:id_event>/", views.list_tickets, name="list_ticket"),
-    path("ticket_export/<str:id_ticket>/", views.export_ticket, name="ticket_export")
+    path("list_ticket_generate/", views.list_ticket_generate, name="ticket_list"),
+    path("generate_ticket/<str:id_ticket>/", views.export_ticket, name="export_ticket"),
+    path("list_tickets/<int:id_event>/", views.list_ticket, name="list_ticket"),
+    
+    # Users
+    path("list_users/", views.list_users, name="list_users"),
+    path("register_user/", views.register_users, name="register_user"),
+    path("update_user/<int:id_user>/", views.update_user, name="update_user"),
+    path("delete_user/<int:id_user>/", views.delete_user, name="delete_user"),
 ]
